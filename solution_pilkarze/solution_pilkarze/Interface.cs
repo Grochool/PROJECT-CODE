f4f1da65afc6a569f4fb394976ea88ee9f246ad8
KrystianB1 <krystian.bakowskiv8@wp.pl>
2018-05-16 20:11:52 +0200

    Update class Liga and Interface
    Dodanie klasy Liga oraz interfejsu

---
+++ solution_pilkarze/solution_pilkarze/Interface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solution_pilkarze
+{
+    interface Interface
+    {
+        void awans(Liga liga);
+        void spadek(Liga liga);
+        void roznica(Liga liga);
+        void zwyciestwapowsredniej(Liga liga);
+        void iloscbramek(List<Liga> list);
+        void straconebramkinamecz(Liga liga);
+
+    }
+}
