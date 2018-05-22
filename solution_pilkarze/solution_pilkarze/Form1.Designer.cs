971a78d90b294b2ecbf87d56b614838c0a591fba
Agnieszka <agnszn002@utp.edu.pl>
2018-05-22 20:33:32 +0200

    jak coś popsułam to sorry

--- solution_pilkarze/solution_pilkarze/Form1.Designer.cs
+++ solution_pilkarze/solution_pilkarze/Form1.Designer.cs
@@ -28,26 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.liga = new System.Windows.Forms.Label();
+            this.opcje = new System.Windows.Forms.Label();
+            this.wyswietl = new System.Windows.Forms.Button();
+            this.ilosc_bramek = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // comboBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(114, 73);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(24, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "test";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(85, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(85, 98);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(172, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // liga
+            // 
+            this.liga.AutoSize = true;
+            this.liga.Location = new System.Drawing.Point(52, 65);
+            this.liga.Name = "liga";
+            this.liga.Size = new System.Drawing.Size(27, 13);
+            this.liga.TabIndex = 3;
+            this.liga.Text = "Liga";
+            // 
+            // opcje
+            // 
+            this.opcje.AutoSize = true;
+            this.opcje.Location = new System.Drawing.Point(44, 101);
+            this.opcje.Name = "opcje";
+            this.opcje.Size = new System.Drawing.Size(35, 13);
+            this.opcje.TabIndex = 4;
+            this.opcje.Text = "Opcje";
+            // 
+            // wyswietl
+            // 
+            this.wyswietl.Location = new System.Drawing.Point(182, 160);
+            this.wyswietl.Name = "wyswietl";
+            this.wyswietl.Size = new System.Drawing.Size(75, 23);
+            this.wyswietl.TabIndex = 5;
+            this.wyswietl.Text = "Wyświetl";
+            this.wyswietl.UseVisualStyleBackColor = true;
+            this.wyswietl.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ilosc_bramek
+            // 
+            this.ilosc_bramek.Location = new System.Drawing.Point(182, 213);
+            this.ilosc_bramek.Name = "ilosc_bramek";
+            this.ilosc_bramek.Size = new System.Drawing.Size(75, 23);
+            this.ilosc_bramek.TabIndex = 6;
+            this.ilosc_bramek.Text = "Ilość bramek";
+            this.ilosc_bramek.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ilosc_bramek);
+            this.Controls.Add(this.wyswietl);
+            this.Controls.Add(this.opcje);
+            this.Controls.Add(this.liga);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Piłkarze";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,7 +111,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label liga;
+        private System.Windows.Forms.Label opcje;
+        private System.Windows.Forms.Button wyswietl;
+        private System.Windows.Forms.Button ilosc_bramek;
     }
 }
 
--- solution_pilkarze/solution_pilkarze/Form1.cs
+++ solution_pilkarze/solution_pilkarze/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace solution_pilkarze
 {
@@ -15,6 +16,37 @@ namespace solution_pilkarze
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Primera Division");
+            comboBox1.Items.Add("Bundesliga");
+            comboBox1.Items.Add("Premier League");
+            comboBox1.Items.Add("Ligue 1");
+
+            comboBox2.Items.Add("Awans");
+            comboBox2.Items.Add("Spadek");
+            comboBox2.Items.Add("Różnica");
+            comboBox2.Items.Add("Zwycięstwa powyżej średniej");
+            comboBox2.Items.Add("Stracone bramki na mecz");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string messageBoxText = "Czy jesteś pewny?";
+            string caption = "Zapytajnik";
+            MessageBoxButtons button = MessageBoxButtons.YesNo;
+            MessageBoxIcon icon = MessageBoxIcon.Information;
+            MessageBox.Show(messageBoxText, caption, button, icon);
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
